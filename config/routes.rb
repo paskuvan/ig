beab4890b834beb35b1e1a18d7d51d0811b8d608
paskuvan <majopaskuvan@gmail.com>
2018-07-29 21:49:32 -0400

    modal

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users,
     path: '',
     path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
-    controllers: {omniauth_callbacks: 'omniauth_callbacks'}
- 
+    controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+
+  #   get '/users/:id', to: 'users#show'
+ 	# /users/1 -> Users controller, show action, params {id: '1'}
+ 	resources :users, only: [:show]
 end
