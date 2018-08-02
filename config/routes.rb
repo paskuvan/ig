a486a63ca46da809044b4c1b94cf5f6d3b0a25ad
paskuvan <majopaskuvan@gmail.com>
2018-08-02 19:52:55 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users,
     path: '',
-    path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+    path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registrations'},
     controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
 
   #   get '/users/:id', to: 'users#show'
