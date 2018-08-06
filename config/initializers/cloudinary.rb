23ee81c78c1339d071dcbb90d82eb6b684722c98
paskuvan <majopaskuvan@gmail.com>
2018-08-05 21:04:28 -0400

    api key ok

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,5 +1,5 @@
 Cloudinary.config do |config|
-	config.cloud_name = "dztlgxwhc"
-	config.api_key = "971284351881728"
-	config.api_secret = "9wLBwHJEIEs2dRlW9wXZw0eKsBQ"
+	config.cloud_name = ENV["cloudinary_cloud_name"]
+	config.api_key = ENV["cloudinary_api_key"]
+	config.api_secret = ENV["cloudinary_api_secret"]
 	config.cdn_subdomain = true
