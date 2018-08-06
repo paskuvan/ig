2fc75a0e19fece2d8b7fdb292d8c0a4a03d4525b
paskuvan <majopaskuvan@gmail.com>
2018-08-05 20:49:00 -0400

    cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,5 @@
+Cloudinary.config do |config|
+	config.cloud_name = "dztlgxwhc"
+	config.api_key = "971284351881728"
+	config.api_secret = "9wLBwHJEIEs2dRlW9wXZw0eKsBQ"
+	config.cdn_subdomain = true
